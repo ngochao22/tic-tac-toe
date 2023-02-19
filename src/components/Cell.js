4a5c5d3ebe997aaae12686e02a12f90399f6130a
ngochao22 <ngochao220903@gmail.com>
2023-02-19 20:49:35 +0700

    Tic-Tac-Toe

---
+++ src/components/Cell.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Cell = ({ value, onClick }) => {
+    return (
+        <div className="game-cell" onClick={onClick}>
+            {value}
+        </div>
+    );
+};
+
+export default Cell;
---
+++ src/components/Game.js
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import Board from "./Board";
+import "../App.css";
+import { calcWinner } from "../helper";
+
+const Game = () => {
+    const [board, setBoard] = useState(Array(9).fill(null));
+    const [xIsNext, setXIsNext] = useState(true);
+    const winner = calcWinner(board);
+    const handleClick = (index) => {
+        const boardCopy = [...board];
+        if (winner || boardCopy[index]) return;
+
+        boardCopy[index] = xIsNext ? "X" : "O";
+
+        setBoard(boardCopy);
+        setXIsNext(!xIsNext);
+    };
+    const handleResetGame = () => {
+        setBoard(Array(9).fill(null));
+    };
+    return (
+        <div>
+            <Board cells={board} onClick={handleClick}></Board>
+            <div className="game-title">
+                {winner ? `Winner is ${!xIsNext ? "X" : "O"}` : ""}
+            </div>
+            <button onClick={handleResetGame}>Reset Game</button>
+        </div>
+    );
+};
+
+export default Game;
